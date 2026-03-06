1cbe9f47d6ff6b28bbae0969e97e45c703137ea5
Alex Pascal <futupas@gmail.com>
2026-03-06 14:57:35 +0100

    upd: fix 3.2 bug

--- handlers/logic_3.php
+++ handlers/logic_3.php
@@ -4,12 +4,22 @@ $scores = ['R'=>0, 'I'=>0, 'S'=>0, 'K'=>0, 'P'=>0, 'A'=>0];
 $userAnswers = [];
 
 foreach ($testData['questions'] as $q) {
-    $val = $_POST["q_" . $q['id']] ?? null;
-    if ($val && isset($scores[$val])) {
+    $qId = $q['id'];
+    $val = $_POST["q_$qId"] ?? null;
+    
+    if ($val) {
         $scores[$val]++;
-        // Find the text for this choice
-        $text = ($q['a']['type'] == $val) ? $q['a']['text'] : $q['b']['text'];
-        $userAnswers["Пара " . $q['id']] = mb_strimwidth($text, 0, $n, "...");
+        
+        // Match the selected value (type) to the text
+        if ($q['a']['type'] == $val) {
+            $text = $q['a']['text'];
+        } else {
+            $text = $q['b']['text'];
+        }
+        
+        $userAnswers["Пара $qId"] = mb_strimwidth($text, 0, $n, "...");
+    } else {
+        $userAnswers["Пара $qId"] = "Не обрано";
     }
 }
 
@@ -18,6 +28,7 @@ $topTypeKey = array_key_first($scores);
 $res = $testData['results'][$topTypeKey] ?? null;
 
 $packedResult['user_answers'] = $userAnswers;
+$packedResult['result_key'] = $topTypeKey;
 $packedResult['result_name'] = $res['name'] ?? 'Unknown';
 $packedResult['result_description'] = $res['description'] ?? '';
 $packedResult['scores'] = $scores;
