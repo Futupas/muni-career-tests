4c0a7325eb4e65b952fa60f2b0a4af031b6d0311
Alex Pascal <futupas@gmail.com>
2026-03-06 15:42:38 +0100

    upd: trying to fix 4.3, 4.1, 2.5 bugs

--- handlers/logic_5.php
+++ handlers/logic_5.php
@@ -1,33 +1,39 @@
 <?php
 $n = (int)($_ENV['QUESTION_TRUNCATE_LEN'] ?? 50);
-$scores = [];
+$results = [];
 $userAnswers = [];
 
+// Populate User Answers
 foreach ($testData['questions'] as $q) {
     $qId = $q['id'];
     $val = $_POST["q_$qId"] ?? '0';
     $userAnswers[mb_strimwidth($q['text'], 0, $n, "...")] = ($val == '1' ? 'Так' : 'Ні');
 }
 
+// Calculate Scores and map to interpretations from JSON
 foreach ($testData['scales'] as $code => $scaleData) {
-    $scores[$code] = 0;
+    $score = 0;
     foreach ($scaleData['keys'] as $qId) {
-        if (($_POST["q_$qId"] ?? '') == '1') $scores[$code]++;
+        if (($_POST["q_$qId"] ?? '') == '1') $score++;
     }
     
-    // Map score to description from JSON
+    // Determine level based on the JSON levels array
     $levelText = "";
     foreach ($testData['levels'] as $lvl) {
-        if ($scores[$code] >= $lvl['min'] && $scores[$code] <= $lvl['max']) {
+        if ($score >= $lvl['min'] && $score <= $lvl['max']) {
             $levelText = $lvl['text'];
             break;
         }
     }
-    $scores[$code] = ['score' => $scores[$code], 'level' => $levelText];
+    
+    $results[$code] = [
+        'name' => $scaleData['name'],
+        'score' => $score,
+        'level' => $levelText,
+        'description' => $scaleData['description']
+    ];
 }
 
 $packedResult['user_answers'] = $userAnswers;
-$packedResult['result_data'] = $scores;
-$packedResult['result_name'] = "Результати: Типи мислення";
-$packedResult['result_description'] = "Обробку завершено. Деталі у result_data.";
+$packedResult['results'] = $results;
 ?>
